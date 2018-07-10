7038d7e8fffcd07a39d12a44440f1e3190418e89
qiang <530522022@qq.com>
2018-07-10 18:34:10 +0800

    。。。。。。

---
+++ app/src/main/java/refresh/qiang/com/myrefreshlayout/refreshLayout/FootView.java
@@ -0,0 +1,34 @@
+package refresh.qiang.com.myrefreshlayout.refreshLayout;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FootView extends View {
+    public FootView(Context context) {
+        this(context,null);
+    }
+
+    public FootView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public FootView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+    private void initView(){
+
+    }
+
+
+
+
+
+
+
+
+
+}
